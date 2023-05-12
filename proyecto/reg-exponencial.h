355dba0e0ff1af306baa8e813638a8c13441ed85
Scalaptia <fernando@haroware.com>
2023-05-12 13:44:59 -0700

    avance 1 inciso e

--- proyecto/reg-exponencial.h
+++ proyecto/reg-exponencial.h
@@ -36,10 +36,68 @@ float sumatoriaYprima(int npuntos) {
     return sumy;
 }
 
+float promedioX(int npuntos) {
+    float promX;
+    promX = sumatoriaX(npuntos) / npuntos;
+    return promX;
+}
+
+float promedioYprima(int npuntos) {
+    float promYprima;
+    promYprima = sumatoriaYprima(npuntos) / npuntos;
+    return promYprima;
+}
+
+float sumatoriaXcuadrado(int npuntos) {
+    float sumx2 = 0;
+    for(int i = 0; i < npuntos; i++) {
+        sumx2 = sumx2 + (fx(i) * fx(i));
+    }
+    return sumx2;
+}
+ 
+float sumatoriaXYprima(int npuntos) {
+    float sumXYprima=0;
+    for(int i = 0; i < npuntos; i++) {
+        sumXYprima = sumXYprima + (fx(i) * fyprima(i));
+    }
+    return sumXYprima;
+}
+
+float obtenerB(int npuntos) {
+    float b, sumXYprima, sumX, sumYprima, sumX2;
+    b = obtenerB(npuntos);
+    sumX = sumatoriaX(npuntos);
+    sumYprima = sumatoriaYprima(npuntos);
+    sumX2 = sumatoriaXcuadrado(npuntos);
+    b = ((npuntos * sumXYprima) - (sumX * sumYprima)) / ((npuntos * sumX2) - (sumX * sumX));
+    return b;
+}
+
+float obtenerAprima(int npuntos) {
+    float b, aprima, promX, promYprima;
+    b = obtenerB(npuntos);
+    promX = promedioX(npuntos);
+    promYprima = promedioYprima(npuntos);
+    aprima = promYprima - (b * promX);
+    return aprima;
+}
+
+void mostrarEcuacion(int npuntos) {
+    printf("SumX= %f \n", sumatoriaX(npuntos));
+    printf("SumY'= %f \n", sumatoriaYprima(npuntos));
+    printf("SumXY'= %f \n", sumatoriaXYprima(npuntos));
+    printf("SumX^2= %f \n", sumatoriaXcuadrado(npuntos));
+    printf("PromX= %f \n", promedioX(npuntos));
+    printf("PromY'= %f \n", promedioYprima(npuntos));
+    printf("B= %f \n", obtenerB(npuntos));
+    printf("A'= %f \n", obtenerAprima(npuntos));
+    printf("Ecuacion: f(x)= %fe^%fx \n", obtenerAprima(npuntos), obtenerB(npuntos));
+}
+
 void regresion_exponencial(int npuntos) {
     printf("\nRegresion exponencial\n");
     calcularYprima(npuntos);
     showTablaXYprima(npuntos);
-    printf("\nSumatoriaX = %f\n", sumatoriaX(npuntos));
-    printf("SumatoriaYprima = %f\n", sumatoriaYprima(npuntos));
+    mostrarEcuacion(npuntos);
 }
