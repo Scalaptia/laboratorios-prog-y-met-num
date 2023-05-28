47d12828b0c1dc080c5e6bdef7a3b31ac139b80c
Scalaptia <fernando@haroware.com>
2023-05-28 13:46:46 -0700

    cambia orden de ejecución simpson combinado

--- proyecto/simpson_combinado.h
+++ proyecto/simpson_combinado.h
@@ -6,19 +6,17 @@ void simpsoncombinado (int npuntos) {
     int a, b;
     int n = npuntos - 3;
     int k = npuntos - 4;
+    
+    a = fx(0);
+    b = fx(k);
+    area1 = simpson1tercio(a, b, n);
 
     a = fx(k);
 	b = fx(npuntos-1);
     area2 = simpson3octavos(a, b, k);
-    
-	npuntos = k+1;
-    b = a;
-    a = fx(0);
-    area1 = simpson1tercio(a, b, npuntos);
 
     areaTotal = area1 + area2;
 
     printf("\nMetodo de simpson combinado\n");
     printf("Area total: %f \n", areaTotal);
-
 }
